f180935cac4a19874c7b40e5e186825636f4c8c4
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-01 22:11:53 +0600

    dynamic query

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { CiUser } from "react-icons/ci";
+import { RiSearchLine } from "react-icons/ri";
 import Show from "./Show";
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  //   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
   useEffect(() => {
     async function get_data() {
-      const res = await fetch("https://api.tvmaze.com/search/shows?q=all");
+      const res = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
       const data = await res.json();
       setData(data);
     }
     get_data();
-  }, []);
+  }, [query]);
   return (
     <>
       <nav className="flex flex-col-reverse gap-y-5 shad rounded-lg py-3 px-5 lg:flex-row justify-around items-center">
@@ -23,6 +27,7 @@ export default function Home() {
               type="text"
               className="bg-gray-200 rounded-md py-2 w-96 px-10"
               placeholder="Search"
+              onChange={(e) => setQuery(e.target.value)}
             />
             <RiSearchLine className="text-gray-400 text-2xl absolute left-2 top-2" />
           </div>
@@ -33,10 +38,6 @@ export default function Home() {
           </button>
         </div>
       </nav>
-
-      
-
-
 
       <div className="grid grid-cols-3 gap-4 p-5">
         {data && data.map((item) => <Show show={item} key={item.show.id} />)}
