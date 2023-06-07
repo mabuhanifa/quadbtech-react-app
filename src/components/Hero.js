40139be28cf72ac22247537bc0562f6e13e3385b
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-07 15:44:11 +0600

    hero

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        
+        <h1 className='text-center my-10'>Best Price to Trade</h1>
+
+
+
+    </div>
+  )
+}
