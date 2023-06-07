6063b65865c98820b781fd3534d1d030bba3d3a0
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-07 18:41:51 +0600

    loading and error states

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,18 +2,38 @@ import React, { useEffect, useState } from "react";
 
 export default function Table() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const res = await fetch("https://crypto-o6vh.onrender.com/api/crypto");
       console.log(res);
-      const data = await res.json();
-      setData(data);
+      const data_ = await res.json();
+      setData(data_);
+      if (data_.length > 0) {
+        setLoading(false);
+      } else {
+        setError(true);
+      }
     };
-
     fetchData();
   }, []);
+  if (loading) {
+    return (
+      <h1 className="text-center text-white font-bold text-2xl my-20">
+        Loading.... Please wait for 30 second for first load
+      </h1>
+    );
+  }
+  if (error) {
+    return (
+      <h1 className="text-center text-red-500 font-bold text-2xl my-20">
+        No Data Found
+      </h1>
+    );
+  }
 
-  console.log(data);
   return (
     <div className="flex justify-center mt-20">
       <div>
@@ -32,21 +52,22 @@ export default function Table() {
                 return (
                   <tr
                     className="px-10  text-gray-100 text-2xl font-bold rounded"
-                    key={index }
+                    key={index}
                   >
-                    <td className="text-center py-4 rad">{index+1}</td>
+                    <td className="text-center py-4 rad">{index + 1}</td>
                     <td className="text-center py-4">{item.name}</td>
                     <td className="text-center py-4">{item.info.last}</td>
                     <td className="text-center py-4">
                       {item.info.buy} / {item.info.sell}{" "}
                     </td>
                     <td className="text-center py-4">
-                     {(Number(item.info.sell - item.info.open) / 100).toFixed(
+                      {(Number(item.info.sell - item.info.open) / 100).toFixed(
                         2
-                      )} %
+                      )}{" "}
+                      %
                     </td>
                     <td className="text-center py-4 rax">
-                    ₹ {(item.info.sell - item.info.open).toFixed(2)}
+                      ₹ {(item.info.sell - item.info.open).toFixed(2)}
                     </td>
                   </tr>
                 );
