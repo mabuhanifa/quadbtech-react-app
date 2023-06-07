6c4c809d8e68801ced4832c8a85f4bf2186226b1
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-07 16:06:37 +0600

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="border-t border-2 mt-20">
+      <div>
+        <p>Copyright © 2019 HodlInfo.com</p>
+        <p>Support</p>
+      </div>
+    </footer>
+  );
+}
